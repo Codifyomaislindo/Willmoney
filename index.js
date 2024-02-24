b06e7e989ab4ad73d55d99938310488ed82f3ac5
EvokeX <156717620+EvokeX@users.noreply.github.com>
2024-02-24 16:48:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         var clickButton = document.querySelector('.btn-primary');
-        clickButton.addEventListener('click', function() {
+        clickButton.addEventListener('click', function () {
             window.location.href = 'https://plracademy.team/';
+        });
+
+        var newButton = document.querySelector('.btn-primary:nth-child(2)');
+        newButton.addEventListener('click', function () {
+            window.location.href = 'https://kiwify.app/JPGh59a?afid=f7Cje3nf';
         });
     });
 </script>
