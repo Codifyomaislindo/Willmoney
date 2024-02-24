cbf8762151f1c90dd9594ca2f53dc6388857babd
EvokeX <156717620+EvokeX@users.noreply.github.com>
2024-02-24 16:37:35 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var clickButton = document.querySelector('.btn-primary');
+        clickButton.addEventListener('click', function() {
+            window.location.href = 'https://plracademy.team/';
+        });
+    });
+</script>
